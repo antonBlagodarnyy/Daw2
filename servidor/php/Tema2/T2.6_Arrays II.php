74e47ee0eb39e3717a4894a8a0fee4c8d96b2c00
antonBlagodarnyy <antonbgi98@gmail.com>
2024-10-27 10:47:42 +0100

    Terminado ej3

--- servidor/php/Tema2/T2.6_Arrays II.php
+++ servidor/php/Tema2/T2.6_Arrays II.php
@@ -29,80 +29,137 @@
             echo "<p><span style=\"font-size: 8em\">&#{$emojis[$iEmoji]};{$pieles[$iPieles]}</span></p>";
             ?>
         </div>
+    </div>
+    <div class="container">
+        <div class="enunciado">
+            <h1>Ejercicio 2</h1>
+            <p>
+                Actualice la página para mostrar una secuencia aleatoria de bits y su complementaria.</p>
+        </div>
+        <div class="ejercicio">
+            <?php
 
-        <div class="container">
-            <div class="enunciado">
-                <h1>Ejercicio 2</h1>
-                <p>
-                    Actualice la página para mostrar una secuencia aleatoria de bits y su complementaria.</p>
+            $bits = [];
 
-                <div class="ejercicio">
-                    <?php
+            for ($i = 0; $i < 10; $i++)
+                array_push($bits, rand(0, 1));
 
-                    $bits = [];
+            var_dump($bits);
 
-                    for ($i = 0; $i < 10; $i++)
-                        array_push($bits, rand(0, 1));
+            for ($i = 0; $i < count($bits); $i++) {
+                if ($bits[$i] == 0) {
+                    $bits[$i] = 1;
+                } else {
+                    $bits[$i] = 0;
+                }
+            }
 
-                    var_dump($bits);
+            var_dump($bits);
 
-                    for ($i = 0; $i < count($bits); $i++) {
-                        if ($bits[$i] == 0) {
-                            $bits[$i] = 1;
-                        } else {
-                            $bits[$i] = 0;
-                        }
-                    }
+            ?>
+        </div>
+    </div>
 
-                    var_dump($bits);
+    <div class="container">
+        <div class="enunciado">
+            <h1>Ejercicio 3</h1>
+            <p>
+                Escriba un programa que enfrente a dos jugadores tirando una serie de dados al azar entre 3 y 9 e indique el resultado. Los dados se comparan en orden (el primero con el primero, el segundo con el segundo, etc.) y gana el jugador que obtenga el número más alto.
+                Mostrar un resumen con cuántas veces ha ganado cada jugador y en conjunto qué jugador ha ganado.
+                NOTA: A la hora de mostrar los dados de cada jugador utiliza la estructura foreach
+            </p>
+        </div>
+        <div class="ejercicio">
 
-                    ?>
-                </div>
+            <?php
+            $veces = rand(3, 9);
 
-                <div class="container">
-                    <div class="enunciado">
-                        <h1>Ejercicio 3</h1>
-                        <p>
-                            Escriba un programa que enfrente a dos jugadores tirando una serie de dados al azar entre 3 y 9 e indique el resultado. Los dados se comparan en orden (el primero con el primero, el segundo con el segundo, etc.) y gana el jugador que obtenga el número más alto.
-                            Mostrar un resumen con cuántas veces ha ganado cada jugador y en conjunto qué jugador ha ganado.
-                            NOTA: A la hora de mostrar los dados de cada jugador utiliza la estructura foreach
-                        </p>
-                    </div>
-                    <div class="ejercicio">
-                        <?php
-                        $veces = rand(3,9);
+            $jugador1 = [];
 
-                        $jugador1 = [];
+            for ($i = 0; $i < $veces; $i++)
+                array_push($jugador1, rand(1, 6));
 
-                        for ($i = 0; $i < $veces; $i++)
-                            array_push($jugador1, rand(3, 9));
+            $jugador2 = [];
+            for ($i = 0; $i < $veces; $i++)
+                array_push($jugador2, rand(1, 6));
 
-                        $jugador2 = [];
-                        for ($i = 0; $i < $veces; $i++)
-                            array_push($jugador2, rand(3, 9));
+            $ganaJ1 = 0;
+            $ganaJ2 = 0;
+            $empate = 0;
 
-                        $ganaJ1=0;
-                        $ganaJ2=0;
-                        $empate=0;
+            for ($i = 0; $i < $veces; $i++) {
+                if ($jugador1[$i] > $jugador2[$i])
+                    $ganaJ1++;
+                else if ($jugador1[$i] < $jugador2[$i])
+                    $ganaJ2++;
+                else
+                    $empate++;
+            }
 
-                        for ($i = 0; $i < $veces; $i++) {
-                            if ($jugador1[$i] > $jugador2[$i])
-                                $ganaJ1++;
-                            else if ($jugador1[$i] < $jugador2[$i])
-                                $ganaJ2++;
-                            else
-                                $empate++;
-                        }
+            echo "<p>Jugado 1:</p>";
+            foreach ($jugador1 as $k => $v) {
+                switch ($v) {
+                    case 1:
+                        echo "<img src=\"dados/1.svg\">";
+                        break;
+                    case 2:
+                        echo "<img src=\"dados/2.svg\">";
+                        break;
+                    case 3:
+                        echo "<img src=\"dados/3.svg\">";
+                        break;
+                    case 4:
+                        echo "<img src=\"dados/4.svg\">";
+                        break;
+                    case 5:
+                        echo "<img src=\"dados/5.svg\">";
+                        break;
+                    case 6:
+                        echo "<img src=\"dados/6.svg\">";
+                        break;
+                    default:
+                        echo "error en switch
+                                    valor:  $v";
+                }
+            }
 
-                       foreach ($jugador1 as $k => $v) {
-                        switch($v){
-                            case 1:
-                                echo "<img src"
-                        }
-                       }
-                        ?>
+            echo "<p>Jugado 2:</p>";
+            foreach ($jugador2 as $k => $v) {
+                switch ($v) {
+                    case 1:
+                        echo "<img src=\"dados/1.svg\">";
+                        break;
+                    case 2:
+                        echo "<img src=\"dados/2.svg\">";
+                        break;
+                    case 3:
+                        echo "<img src=\"dados/3.svg\">";
+                        break;
+                    case 4:
+                        echo "<img src=\"dados/4.svg\">";
+                        break;
+                    case 5:
+                        echo "<img src=\"dados/5.svg\">";
+                        break;
+                    case 6:
+                        echo "<img src=\"dados/6.svg\">";
+                        break;
+                    default:
+                        echo "error en switch
+                                    valor:  $v";
+                }
+            }
+            echo "<p>El jugador 1 ha ganado {$ganaJ1} veces, el jugador 2 ha ganado {$ganaJ2} veces y los jugadores han empatado {$empate} veces.</p>";
 
-                    </div>
+            if ($ganaJ1 > $ganaJ2)
+                echo "En conjunto ha ganado el jugador 1.";
+            else if ($ganaJ2 > $ganaJ1)
+                echo "En conjunto ha ganado el jugador 2.";
+            else
+                echo "En conjunto, los jugadores han empatado."
+            ?>
+
+        </div>
 </body>
 
 </html>
