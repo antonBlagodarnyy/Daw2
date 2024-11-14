c9e6553e2c142acf2f84d3a027466ccf2a184316
antonBlagodarnyy <antonbgi98@gmail.com>
2024-11-14 10:17:16 +0100

    Terminado ej17

--- cliente/Boletin1/scripts/Ej17.js
+++ cliente/Boletin1/scripts/Ej17.js
@@ -1,5 +1,13 @@
 const output = document.getElementById("output");
 
+//Los parentesis sirven para obtener esos valores dentro del array que devuelve exec
+const nombreReg = /Nombre:\s([a-zA-ZÁ-ÿ]+)\s([a-zA-ZÁ-ÿ]+)/;
+const numCuentaReg = /Número\sde\scuenta:\s(\d{4}-\d{4}-\d{4}-\d{4}-\d{4})/;
+const correoReg =
+  /Correo\selectrónico:\s[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}/;
+const transaccionReg = /Transacción\s\d+:\s(\d+)/g;
+const transaccionPalabraReg = /Transacción/g;
+
 const texto = `
 Nombre: Juan Pérez
 Número de cuenta: 1234-5678-9876-5432-1234
@@ -13,35 +21,59 @@ tu saldo es 950€ y la próxima transacción es de
 300€.`;
 
 function extraerNombreCliente() {
-    const nombreReg = /Nombre:\s([a-zA-ZÁ-ÿ]+)\s([a-zA-ZÁ-ÿ]+)/;
+  let nombreObj = nombreReg.exec(texto);
 
-    let nombreObj = nombreReg.exec(texto);
+  let nombre = `${nombreObj[1]} ${nombreObj[2]}`;
 
-    let nombre = `${nombreObj[1]} ${nombreObj[2]}`;
-
-    output.innerHTML = `<p>${nombre}</p>`;
+  output.innerHTML = `<p>${nombre}</p>`;
 }
 
 function validarNumeroDeCuenta() {
-    const numCuentaReg = /Número\sde\scuenta: (\d{4}-\d{4}-\d{4}-\d{4}-\d{4})/;
-
-    /* let numCuentaObj = numCuentaReg.exec(texto);
-    let numCuenta = numCuentaObj[0].slice("Número de cuenta: ".length)
-    output.innerHTML = `<p>${numCuenta}</p>`; */
-
-    output.innerHTML = numCuentaReg.test(texto) ? "<p>Numero de cuenta valido.</p>" : "<p>Numero de cuenta no valido.</p>"
+  output.innerHTML = numCuentaReg.test(texto)
+    ? "<p>Numero de cuenta valido.</p>"
+    : "<p>Numero de cuenta no valido.</p>";
 }
 
 function validarCorreo() {
-    const correoReg = /Correo\selectrónico:\s[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}/;
-
-    output.innerHTML = correoReg.test(texto) ? "<p>Correo valido.</p>" : "<p>Correo no valido.</p>"
+  output.innerHTML = correoReg.test(texto)
+    ? "<p>Correo valido.</p>"
+    : "<p>Correo no valido.</p>";
 }
 
-function sumarTransacciones(){
-const transaccionReg = /Transacción\s\d+:\s(\d+)€/g;
+function sumarTransacciones() {
+  let transaccionObj = texto.match(transaccionReg);
 
-let transaccionObj = texto.match(transaccionReg);
+  let sumatorio = 0;
 
-console.log(transaccionObj)
+  //Recorro todas las transacciones
+  for (var prop in transaccionObj) {
+    //Evita recorrer las propiedades heredadas de prototipos
+    if (Object.prototype.hasOwnProperty.call(transaccionObj, prop)) {
+      //Suma el parseado a int
+      sumatorio += parseInt(
+        //Accedo a la propiedad, split recorta el string y devuelve un array, por eso accedo al puesto [1]
+        transaccionObj[prop].split(/Transacción\s\d+:\s/)[1]
+      );
+    }
+  }
+
+  output.innerHTML = `<p>Total de las transacciones: ${sumatorio}€</p>`;
+}
+
+function cifrarNumeroCuenta() {
+  let numCuentaObj = numCuentaReg.exec(texto);
+
+  let numCuentaSinCifrar = numCuentaObj[1];
+
+  let maskedPart = numCuentaSinCifrar.slice(0, -5).replace(/\d/g, "*");
+  let visiblePart = numCuentaSinCifrar.slice(-5); // Last 5 characters (includes the last hyphen and 4 digits)
+
+  let numCuenta = maskedPart + visiblePart;
+
+  output.innerHTML = `<p>Numero cuenta cifrado: ${numCuenta}</p>`;
+}
+
+function contarPalabraTransaccion(){
+    let transaccionObj = texto.match(transaccionPalabraReg);
+    output.innerHTML = `<p>La palabra transaccion aparece ${transaccionObj.length} veces.</p>`
 }
