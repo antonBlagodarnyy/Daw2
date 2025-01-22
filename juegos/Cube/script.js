e4963a4dc81c3b062b5c73fb39affbb874ab0b0a
antonBlagodarnyy <104981469+antonBlagodarnyy@users.noreply.github.com>
2025-01-22 17:21:58 +0100

    juego

--- juegos/Cube/script.js
+++ juegos/Cube/script.js
@@ -3,22 +3,47 @@ import * as THREE from "https://unpkg.com/three@0.142.0/build/three.module.js";
 let range = document.getElementById("rango");
 
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
 const renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
-document.body.appendChild( renderer.domElement );
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
 
-const geometry = new THREE.BoxGeometry( 2, 2, 2 ); 
-const edges = new THREE.EdgesGeometry( geometry ); 
-const line = new THREE.LineSegments(edges, new THREE.LineBasicMaterial( { color: "green" } ) ); 
-scene.add( line );
+let cubes = [];
+
+function createCubes(w, h, d) {
+	let geometry = new THREE.BoxGeometry(w, h, d);
+	let edges = new THREE.EdgesGeometry(geometry);
+	let line = new THREE.LineSegments(edges, new THREE.LineBasicMaterial({ color: "green" }));
+
+	cubes.push(line);
+}
+
+for (let i = 0; i < 5; i++) {
+	createCubes(i, i, i);
+}
+
+cubes.forEach(cube => {
+	scene.add(cube);
+});
+
+
 
 camera.position.z = 5;
 
 function animate() {
-	renderer.render( scene, camera );
-line.rotation.x += parseFloat(range.value);
-line.rotation.y += parseFloat(range.value);
+	renderer.render(scene, camera);
+
+for (let i = 0; i < cubes.length; i++) {
+	const cube = cubes[i];
+	if(i%2==0){
+		cube.rotation.x += parseFloat(range.value);
+		cube.rotation.y += parseFloat(range.value);
+	}else{
+		cube.rotation.x -= parseFloat(range.value);
+		cube.rotation.y -= parseFloat(range.value);
+	}
+
 }
-renderer.setAnimationLoop( animate );
+}
+renderer.setAnimationLoop(animate);
